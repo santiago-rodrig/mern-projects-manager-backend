06c386b1403dc8c82c0ebc7e9413cf6eb8e0620c
Santiago <santo1996.29@gmail.com>
2020-11-22 12:21:48 -0400

    setup: express server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("From index.js")
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
+})
