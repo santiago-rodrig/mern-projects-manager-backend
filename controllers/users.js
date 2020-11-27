5c2b293b2a0eb1ad67204b580e070c622c8d5ce9
Santiago Rodriguez <santo1996.29@gmail.com>
2020-11-27 16:36:20 -0400

    feature: handle users creation

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,12 @@
-exports.createUser = (req, res) => {
-    console.log(req.body)
+const User = require('../models/User')
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = new User(req.body)
+        await user.save()
+        res.send("User was successfully created")
+    } catch (error) {
+        console.log(error)
+        res.status(400).send("Something went wrong")
+    }
 }
