95496a55f4eed7fbf1f09e4377a5b7a40c227ac2
Santiago Rodriguez <santo1996.29@gmail.com>
2020-11-27 17:59:17 -0400

    feature: return jwt after user creation

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const bcryptjs = require('bcryptjs')
 const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
 exports.createUser = async (req, res) => {
     const { email, password } = req.body
     const errors = validationResult(req)
@@ -16,7 +17,13 @@ exports.createUser = async (req, res) => {
         const salt = await bcryptjs.genSalt(10)
         user.password = await bcryptjs.hash(password, salt)
         await user.save()
-        return res.json({ msg: 'User was successfully created' })
+        const payload = { user: user.id }
+        jwt.sign(payload, process.env.SECRET_KEY, {
+            expiresIn: 3600
+        }, (error, token) => {
+            if (error) throw error
+            return res.json({ token })
+        })
     } catch (error) {
         console.log(error)
         return res.status(400).json({ msg: 'Something went wrong' })
