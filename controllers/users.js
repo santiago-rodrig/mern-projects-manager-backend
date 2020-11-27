3e534338680bc202191c216d1e236ae40bc8216c
Santiago Rodriguez <santo1996.29@gmail.com>
2020-11-27 17:27:13 -0400

    feature: validate fields at user creation

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,24 @@
 const User = require('../models/User')
-
+const bcryptjs = require('bcryptjs')
+const { validationResult } = require('express-validator')
 exports.createUser = async (req, res) => {
-    const { email } = req.body
-
+    const { email, password } = req.body
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
     try {
         let user = await User.findOne({ email })
-
         if (user) {
             return res.status(400).json({ msg: 'Email is already taken' })
         }
-
         user = new User(req.body)
-
+        const salt = await bcryptjs.genSalt(10)
+        user.password = await bcryptjs.hash(password, salt)
         await user.save()
-
         return res.json({ msg: 'User was successfully created' })
     } catch (error) {
         console.log(error)
-
         return res.status(400).json({ msg: 'Something went wrong' })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const usersController = require('../controllers/users')
 const router = express.Router()
-
-router.post('/', usersController.createUser)
-
+const { check } = require('express-validator')
+// The root of this router is /api/users
+router.post('/', [
+    check('name', 'The name is required').not().isEmpty(),
+    check('email', 'Email is invalid').isEmail(),
+    check('password', 'Password must be at least 6 characters long').isLength({ min: 6 })
+], usersController.createUser)
 module.exports = router
