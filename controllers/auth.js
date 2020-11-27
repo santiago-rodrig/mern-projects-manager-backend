1204adf398222dc68b8d22a94edc5650e5e4bafe
Santiago Rodriguez <santo1996.29@gmail.com>
2020-11-27 18:41:39 -0400

    feature: implement user authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,31 @@
-// const User = require('../models/User')
-// const bcryptjs = require('bcryptjs')
+const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
 const { validationResult } = require('express-validator')
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 exports.authenticateUser = async (req, res) => {
-    // const { email, password } = req.body
+    const { email, password } = req.body
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
     try {
-        // TODO
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ msg: 'Inexistent user' })
+        }
+        const validPassword = await bcryptjs.compare(password, user.password)
+        if (!validPassword) {
+            return res.status(400).json({ msg: 'Wrong password' })
+        }
+        const payload = { user: user.id }
+        jwt.sign(payload, process.env.SECRET_KEY, {
+            expiresIn: 3600
+        }, (error, token) => {
+            if (error) throw error
+            return res.json({ token })
+        })
     } catch (error) {
-        // TODO
+        console.log(error)
+        return res.status(400).json({ msg: 'Something went wrong' })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const usersController = require('../controllers/users')
 const router = express.Router()
 const { check } = require('express-validator')
-// The root of this router is /api/users
+// The root of this route is /api/users
 router.post('/', [
     check('name', 'The name is required').not().isEmpty(),
     check('email', 'Email is invalid').isEmail(),
