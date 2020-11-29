dc5c5f4949a58270ecd9a473e486e906d77a2f82
Santiago Rodriguez <santo1996.29@gmail.com>
2020-11-29 09:00:07 -0400

    add validation for project name

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,5 +1,10 @@
 const Project = require('../models/Project')
+const { validationResult } = require('express-validator')
 exports.createProject = async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
     try {
         const project = new Project(req.body)
         await project.save()
--- routes/projects.js
+++ routes/projects.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 const projectsController = require('../controllers/projects')
 const auth = require('../middleware/auth')
+const { check } = require('express-validator')
 // This router has as root /api/projects
-router.post('/', auth, projectsController.createProject)
+router.post(
+    '/',
+    auth,
+    [check('name', 'The project must have a nonempty name').not().isEmpty()],
+    projectsController.createProject
+)
+// TODO
+// router.get('/', authprojectsController.getProjects)
 module.exports = router
