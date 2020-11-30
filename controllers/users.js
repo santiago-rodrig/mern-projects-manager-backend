45c8a2a4b906b7a67be240c3b87bae6012b1dd5d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-11-30 09:35:50 -0400

    add separation of code blocks

--- controllers/users.js
+++ controllers/users.js
@@ -2,22 +2,28 @@ const User = require('../models/User')
 const bcryptjs = require('bcryptjs')
 const { validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
+
 exports.createUser = async (req, res) => {
     const { email, password } = req.body
     const errors = validationResult(req)
+
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
+
     try {
         let user = await User.findOne({ email })
+
         if (user) {
             return res.status(400).json({ msg: 'Email is already taken' })
         }
+
         user = new User(req.body)
         const salt = await bcryptjs.genSalt(10)
         user.password = await bcryptjs.hash(password, salt)
         await user.save()
         const payload = { user: user.id }
+
         jwt.sign(
             payload,
             process.env.SECRET_KEY,
@@ -26,7 +32,8 @@ exports.createUser = async (req, res) => {
             },
             (error, token) => {
                 if (error) throw error
-                return res.json({ token })
+
+                res.json({ token })
             }
         )
     } catch (error) {
